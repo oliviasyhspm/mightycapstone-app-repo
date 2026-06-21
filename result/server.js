a3056ebf94b08c4da4437961596b2e9bdb6aa5c1
oliviasiow <olivia.siow.y@gmail.com>
2026-06-22 05:44:19 +0800

    Update server.js

--- result/server.js
+++ result/server.js
@@ -6,7 +6,7 @@ var express = require('express'),
     server = require('http').Server(app),
     io = require('socket.io')(server);
 
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 80;
 
 io.on('connection', function (socket) {
 
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: 'postgres://postgres:postgres@db-service/postgres'
 });
 
 async.retry(
